a3bcf1b118e3e9f40177a1a0c419849cea485013
mahaddevelpoer <mahadb847@email.com>
2026-08-16 09:48:45 +0500

    Set compileSdk = 36 to satisfy AndroidX dependencies requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.mahadandmehdi.video_origin_analyzer"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
