4d84ab3bc9888949c06c52b4d4092140d1280989
mahaddevelpoer <mahadb847@email.com>
2026-08-28 16:27:17 +0500

    fix: remove unused _formatNumber warning, zero analyzer issues

--- lib/features/analyzer/presentation/analysis_result_screen.dart
+++ lib/features/analyzer/presentation/analysis_result_screen.dart
@@ -1620,14 +1620,4 @@ class _AnalysisResultScreenState extends ConsumerState<AnalysisResultScreen>
       ),
     );
   }
-
-  String _formatNumber(int num) {
-    if (num >= 1000000) {
-      return '${(num / 1000000).toStringAsFixed(1)}M';
-    }
-    if (num >= 1000) {
-      return '${(num / 1000).toStringAsFixed(1)}K';
-    }
-    return num.toString();
-  }
 }
